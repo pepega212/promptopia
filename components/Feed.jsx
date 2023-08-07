3f55a3d99a753d33a4c996be8e0e72b98f667d2c
Dicky Sanjaya <dickysanjaya003@gmail.com>
2023-08-07 23:19:13 +0700

    search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -19,23 +19,51 @@ const PromptCardList = ({ data, handleTagClick }) => {
 }
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('');
   const [posts, setPosts] = useState([]);
+  
+  const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  const handleSearchChange = (e) => {
-
-  }
-
-  useEffect(() => {
-    const fetchPosts = async () => {
+  const fetchPosts = async () => {
       const res = await fetch('/api/prompt');
       const data = await res.json();
 
       setPosts(data);
     }
 
+  useEffect(() => {
     fetchPosts();
   }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search 
+    return posts.filter(
+      (item) => 
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  }
 
   return (
     <section className='feed'>
@@ -49,11 +77,19 @@ const Feed = () => {
           className='search_input peer'
         />
       </form>
+      
 
-      <PromptCardList
-        data={posts}
-        handleTagClick={() => {}}
+      {searchText ? (
+        <PromptCardList
+        data={searchedResults}
+        handleTagClick={handleTagClick}
       />
+      ) : (
+        <PromptCardList
+          data={posts}
+          handleTagClick={handleTagClick}
+        />
+      )}
 
     </section>
   )
